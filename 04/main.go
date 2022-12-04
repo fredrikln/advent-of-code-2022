b036a6861be3deb5c8997fb899f29fd05ceacfa2
Fredrik Larsson <fredrikln@gmail.com>
2022-12-04 11:11:08 +0100

    Finish day 4

--- 04/main.go
+++ 04/main.go
@@ -1,13 +1,106 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/fredrikln/advent-of-code-2022/parsing"
+)
+
 func main() {
-	//
+	input := parsing.ReadRowsToStringSlice("input.txt")
+
+	fmt.Println("Part 1:", part1(input))
+	fmt.Println("Part 2:", part2(input))
 }
 
-func part1() {
-	//
+func mapToInt(values []string) []int {
+	out := make([]int, 0, len(values))
+	for _, value := range values {
+		v, err := strconv.Atoi(value)
+		if err != nil {
+			panic(err)
+		}
+
+		out = append(out, v)
+	}
+
+	return out
 }
 
-func part2() {
-	//
+func checkFullOverlap(a0, a1, b0, b1 int) bool {
+	if b0 >= a0 && b1 <= a1 {
+		return true
+	}
+
+	return false
+}
+
+func checkPairFullOverlap(pair string) bool {
+	elves := strings.Split(pair, ",")
+	a := mapToInt(strings.Split(elves[0], "-"))
+	b := mapToInt(strings.Split(elves[1], "-"))
+
+	if checkFullOverlap(a[0], a[1], b[0], b[1]) || checkFullOverlap(b[0], b[1], a[0], a[1]) {
+		return true
+	}
+
+	return false
+}
+
+func checkPartialOverlap(a0, a1, b0, b1 int) bool {
+	if b0 <= a0 && b1 >= a0 {
+		return true
+	}
+
+	if b0 <= a1 && b1 >= a1 {
+		return true
+	}
+
+	if b0 <= a0 && b1 >= a1 {
+		return true
+	}
+
+	if b0 >= a0 && b1 <= a1 {
+		return true
+	}
+
+	return false
+}
+
+func checkPairPartialOverlap(pair string) bool {
+	elves := strings.Split(pair, ",")
+	a := mapToInt(strings.Split(elves[0], "-"))
+	b := mapToInt(strings.Split(elves[1], "-"))
+
+	if checkPartialOverlap(a[0], a[1], b[0], b[1]) || checkPartialOverlap(b[0], b[1], a[0], a[1]) {
+		return true
+	}
+
+	return false
+}
+
+func part1(pairs []string) int {
+	sum := 0
+
+	for _, pair := range pairs {
+		if checkPairFullOverlap(pair) {
+			sum += 1
+		}
+	}
+
+	return sum
+}
+
+func part2(pairs []string) int {
+	sum := 0
+
+	for _, pair := range pairs {
+		if checkPairPartialOverlap(pair) {
+			sum += 1
+		}
+	}
+
+	return sum
 }
--- 04/main_test.go
+++ 04/main_test.go
@@ -1,1 +1,128 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	input := []string{
+		"2-4,6-8",
+		"2-3,4-5",
+		"5-7,7-9",
+		"2-8,3-7",
+		"6-6,4-6",
+		"2-6,4-8",
+	}
+
+	want := 2
+	got := part1(input)
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	input := []string{
+		"2-4,6-8",
+		"2-3,4-5",
+		"5-7,7-9",
+		"2-8,3-7",
+		"6-6,4-6",
+		"2-6,4-8",
+	}
+
+	want := 4
+	got := part2(input)
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestCheckPairFullOverlap(t *testing.T) {
+	testcases := []struct {
+		pair string
+		want bool
+	}{
+		{
+			"2-4,6-8",
+			false,
+		},
+		{
+			"2-3,4-5",
+			false,
+		},
+		{
+			"5-7,7-9",
+			false,
+		},
+		{
+			"2-8,3-7",
+			true,
+		},
+		{
+			"6-6,4-6",
+			true,
+		},
+		{
+			"2-6,4-8",
+			false,
+		},
+	}
+
+	for i, tc := range testcases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := checkPairFullOverlap(tc.pair)
+			want := tc.want
+
+			if got != want {
+				t.Errorf("got %v want %v", got, want)
+			}
+		})
+	}
+}
+
+func TestCheckPairPartialOverlap(t *testing.T) {
+	testcases := []struct {
+		pair string
+		want bool
+	}{
+		{
+			"2-4,6-8",
+			false,
+		},
+		{
+			"2-3,4-5",
+			false,
+		},
+		{
+			"5-7,7-9",
+			true,
+		},
+		{
+			"2-8,3-7",
+			true,
+		},
+		{
+			"6-6,4-6",
+			true,
+		},
+		{
+			"2-6,4-8",
+			true,
+		},
+	}
+
+	for i, tc := range testcases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := checkPairPartialOverlap(tc.pair)
+			want := tc.want
+
+			if got != want {
+				t.Errorf("got %v want %v", got, want)
+			}
+		})
+	}
+}
